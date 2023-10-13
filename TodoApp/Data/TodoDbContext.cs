c5ae2b43437e631b6a477f6fd78b69f81ef6f79a
TSIAZE KENFACK <tsiazezephirin@gmail.com>
2023-10-13 11:57:58 +0100

    Ajoutez des fichiers projet.

---
+++ TodoApp/Data/TodoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions options):base(options)
+        {
+                
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
